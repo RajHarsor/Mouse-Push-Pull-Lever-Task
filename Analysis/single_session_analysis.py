582c3cc40d5391690ae302ac8ecf32a4bad44072
RajHarsor <121781270+RajHarsor@users.noreply.github.com>
2023-07-25 16:41:54 -0400

    Added dif colors for tick marks for Rewards graph

--- Analysis/single_session_analysis.py
+++ Analysis/single_session_analysis.py
@@ -19,15 +19,14 @@ pathTrial=pathTrial.replace('"','')
 
 upperXBound= int(input("Enter the upper X bound: "))
 LowerXBound= int(input("Enter the lower X bound: "))
-Push= int(input("Enter the value of a push: "))
-Pull= int(input("Enter the value of a pull: "))
+PushValue= int(input("Enter the value of a push: "))
+PullValue= int(input("Enter the value of a pull: "))
 
-#%% Read the files
+#%% Read the files and clean up the files
 #Read the files
 dfCoords = pd.read_csv(pathCoords, sep=',', header=None, on_bad_lines="warn")
-#dfTrial = pd.read_csv(pathTrial, sep=',', header=None, names=['Trial Number', 'Reaction Time', 'Current Array', 'Current Array Push/Pull Ratio', 'Total Push/Pull Ratio', 'Solenoid Open Time', 'Push/Pull', 'ISI Delay'], on_bad_lines="warn")
+dfTrial = pd.read_csv(pathTrial, sep=',', header=None, names=['Trial Number', 'Reaction Time', 'Current Array', 'Current Array Push/Pull Ratio', 'Total Push/Pull Ratio', 'Solenoid Open Time', 'Push/Pull', 'ISI Delay'], on_bad_lines="warn")
 
-#%% Clean up the Coordinates Code
 #Edit index 0, row 0 to delete the values before the t including the t
 #dfCoords.at[0, 0] = dfCoords.at[0, 0].split('\t')[1]
 
@@ -40,6 +39,9 @@ dfCoords.columns = ['X Coordinate', 'Y Coordinate', 'Time (us)', 'Phase']
 #Reset the index
 dfCoords = dfCoords.reset_index(drop=True)
 
+#If any of the values in the Time (us) are the same, delete the row
+dfCoords = dfCoords.drop_duplicates(subset='Time (us)', keep='first')
+
 #Convert the time to minutes
 dfCoords['Time (min)'] = dfCoords['Time (us)']/60000000
 
@@ -50,6 +52,9 @@ dfCoords = dfCoords[['X Coordinate', 'Y Coordinate', 'Phase', 'Time (us)', 'Time
 dfCoords = dfCoords[pd.to_numeric(dfCoords['X Coordinate'], errors='coerce').notnull()]
 dfCoords = dfCoords[pd.to_numeric(dfCoords['Y Coordinate'], errors='coerce').notnull()]
 dfCoords = dfCoords[pd.to_numeric(dfCoords['Time (min)'], errors='coerce').notnull()]
+
+#Any rows in the column dfTrials['Push/Pull'] that do not contain the string 'Push' or 'Pull' will be deleted
+dfTrial = dfTrial[dfTrial['Push/Pull'].str.contains('Push|Pull')]
 
 #Make thew columns X and Y coordinates and Time(us) and Time(min) into floats
 dfCoords['X Coordinate'] = dfCoords['X Coordinate'].astype(int)
@@ -78,7 +83,7 @@ fig1.update_layout(title='Coordinates vs Time', xaxis_title='Time (min)', yaxis_
 #Show the graph
 fig1.show()
 """"
-#%% Matplotlib Graph 
+#%% Coordinates over Time
 #Make matplotlib graph with Time (min) on the x axis and X and Y coordinates on the Y axis
 import matplotlib.pyplot as plt
 
@@ -107,6 +112,8 @@ ax.legend()
 plt.show()
 
 #%% Line Graph - Coordinates over Time Smoothed
+
+""""
 # Apply some smoothing to the X and Y coordinates using the Savitzky-Golay filter
 from scipy.signal import savgol_filter
 
@@ -126,8 +133,18 @@ fig3.update_layout(title='Smoothed Coordinates vs Time', xaxis_title='Time (min)
 
 #Show the graph
 fig3.show()
+"""
 
-#%% Matplotlib Graph (for exporting purposes)
+#%% Matplotlib Graph
+#Apply some smoothing to the X and Y coordinates using the Savitzky-Golay filter
+from scipy.signal import savgol_filter
+
+#Create a new column for the smoothed X Coordinate
+dfCoords['X Coordinate Smoothed'] = savgol_filter(dfCoords['X Coordinate'], 51, 3)
+
+#Create a new column for the smoothed Y Coordinate
+dfCoords['Y Coordinate Smoothed'] = savgol_filter(dfCoords['Y Coordinate'], 51, 3)
+
 #Make matplotlib graph with Time (min) on the x axis and X and Y coordinates smoothed on the Y axis
 import matplotlib.pyplot as plt
 
@@ -156,19 +173,17 @@ ax.legend()
 #Show the graph
 plt.show()
 
+#FIXME The savgol filter for some reason causes these duplicate time spots or something? It's weird
+
 #%% 3D Plot of Coordinates over Time
 # FIXME This 3D plot is not working for some reason, test again later
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 
-#Create Figure
-fig = plt.figure()
-ax = Axes3D(fig)
+#Import plotly express
+import plotly.express as px
 
-#Create Plot
-dfig = ax.scatter(dfCoords['X Coordinate'], dfCoords['Y Coordinate'], dfCoords['Time (min)'], color = 'green', cmap='viridis', linewidth=0.5)
+fig = px.line_3d(x=dfCoords['X Coordinate'], y=dfCoords['Y Coordinate'], z=dfCoords['Time (min)'])
 
-plt.show()
+fig.show()
 
 #%% X Coordinates with Rewards Over Time
 #Import matplotlib
@@ -192,13 +207,12 @@ for index, row in dfCoords.iterrows():
 #Drop columns that are duplicates in the Time (min) column
 dfCoords = dfCoords.drop_duplicates(subset='Time (min)', keep='first')
 
-
 #Plot X Coordinate vs Time (min)
 plt.plot(dfCoords['Time (min)'], dfCoords['X Coordinate'], color='black')
 
 #Draw dotted red lines at the pull and push coordinate values
-plt.axhline(y=Pull, color='red', linestyle='--', alpha = 0.5)
-plt.axhline(y=Push, color='red', linestyle='--', alpha = 0.5)
+plt.axhline(y=PullValue, color='red', linestyle='--', alpha = 0.5)
+plt.axhline(y=PushValue, color='red', linestyle='--', alpha = 0.5)
 
 #Make the image longer
 plt.gcf().set_size_inches(20, 5)
@@ -215,8 +229,25 @@ plt.gca().spines['right'].set_visible(False)
 #Make a list with just the int 550 in it that is the same length as the length of Time
 y = [dfCoords['X Coordinate'].max() + 5] * len(Time)
 
-#Make a scatter plot of the X and Y coordinates where it goes from Stage 2 to Stage 3
-plt.scatter(Time, y, color='blue', marker='|', alpha = 0.5)
+#Make dfTrial['Phase'] into a list
+phase = dfTrial['Push/Pull'].tolist()
+
+phase_colors = []
+#For the values in Phase, if it is a push, make it orange, if it is a pull, make it blue
+for x in phase:
+    if x == ' Push ':
+        phase_colors.append('orange')
+    elif x == ' Pull ':
+        phase_colors.append('blue')
+    else:
+        phase_colors.append('black')
+        
+#Make sure phase_colors is the same length as Time, if it isn't add 'black' until it is
+while len(phase_colors) < len(Time):
+    phase_colors.append('black')
+
+#Make a scatter plot of the X and Y coordinates where it goes from Stage 2 to Stage 3 ands change the color based on phase list
+plt.scatter(Time, y, color=phase_colors, marker='|', alpha = 0.5)
 
 #Add Title
 plt.title('X Coordinate vs Time (min)')
