37eccb0b78ba4d2423ba2c88caf42f2c1277c455
RajHarsor <rdh92@rutgers.edu>
2023-08-28 06:40:05 -0400

    working on analysis gui

--- Analysis/AnalysisGUI.py
+++ Analysis/AnalysisGUI.py
@@ -1,11 +1,33 @@
+#%%
+
 #Import Packages
 import pandas as pd
 import numpy as np
+import inquirer
 import single_session_functions as ssaf
 
-
-#Import and Clean Data/Users/rajharsora/Downloads/B4.1-42_8..22.23_Coordinates.txt
-ssaf.importandclean()
-
-
-
+#Ask the user if they want to do the single session analysis or the multi session analysis
+questions = [
+    inquirer.List('Analysis',
+                    message="Which analysis would you like to perform?",
+                    choices=['Single Session', 'Multi Session'],
+    ),
+]
+answers = inquirer.prompt(questions)
+#%%
+if answers['Analysis'] == 'Single Session':
+#Ask the user whether they want either the Push/Down or Push/Pull Analysis
+    questions = [
+        inquirer.List('Analysis',
+                        message="Which analysis would you like to perform?",
+                        choices=['Push/Down', 'Push/Pull'],
+        ),
+    ]
+    answers = inquirer.prompt(questions)
+#%%
+#Import and Clean Data
+if answers['Analysis'] == 'Push/Down':
+    ssaf.importandclean_Push_Down()
+    
+if answers['Analysis'] == 'Push/Pull':
+    ssaf.importandclean_Push_Pull()
