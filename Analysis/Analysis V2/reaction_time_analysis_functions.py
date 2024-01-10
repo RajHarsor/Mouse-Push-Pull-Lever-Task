0f0fa5070256af0f57db3f8ad9d8ea06af34c0db
RajHarsor <121781270+RajHarsor@users.noreply.github.com>
2024-01-10 18:05:50 -0500

    finished testing in playground

--- Analysis/Analysis V2/reaction_time_analysis_functions.py
+++ Analysis/Analysis V2/reaction_time_analysis_functions.py
@@ -3,7 +3,10 @@ import polars as pl
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import os
+import itertools
 
+#This is only for one day
 def load_trials_data_for_rt_and_plot(path, day):
     df = pd.read_csv(path, names=["Trials Number", 'Reaction Time', 'Array', 'Array Counts', 'Total Counts', 'Solenoid Open Time', 'Movement', 'ISI'])
 
@@ -38,3 +41,24 @@ def load_trials_data_for_rt_and_plot(path, day):
     plt.ylabel("Number of Trials")
 
     print(f"Out of {len(rt)} trials, {len(rt_appended)} trials were less than 10000 ms.")
+    
+def get_trials_files_paths(overall_path):
+    d = {}
+    
+    #Find the trials.txt files in the folder and its subfolders
+    for root, dirs, files in os.walk(overall_path):
+        for file in files:
+            if file.endswith("Trials.txt"):
+                #Get the path to the file
+                path = os.path.join(root, file)
+                #Get the folder two levels above the file
+                parent_folder = os.path.basename(os.path.dirname(os.path.dirname(path)))
+                #Put the path and parent folder into the dictionary
+                d[path] = parent_folder
+    
+    #Split the paths based on the unique values in the dictionary into a list of lists
+    paths = [list(g) for _, g in itertools.groupby(d, d.get)]
+
+    #Seperate each list in the list of lists into a seperate list
+    for i in range(len(paths)):
+        exec(f"paths{i} = paths[i]")
