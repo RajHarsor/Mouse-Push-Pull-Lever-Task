594c06d3093418e33df01df1ef0edaebb1a307e3
RajHarsor <121781270+RajHarsor@users.noreply.github.com>
2024-03-01 11:25:30 -0500

    fixed a print statement

--- Code/everythingscript_clean/src/the_everything_script.ino.cpp
+++ Code/everythingscript_clean/src/the_everything_script.ino.cpp
@@ -1,4 +1,7 @@
-/* #region DEPENDENCIES*/
+# 1 "C:\\Users\\rdh92\\AppData\\Local\\Temp\\tmpogsmtk96"
+#include <Arduino.h>
+# 1 "C:/Users/rdh92/Documents/GitHub/Mouse-Push-Pull-Lever-Task/Code/everythingscript_clean/src/the_everything_script.ino"
+
 #include <Arduino.h>
 #include <vector>
 #include <string>
@@ -15,48 +18,30 @@
 #include "isi_delay_mod_visual.h"
 #include "cue_evoked_sound.h"
 
-/* #endregion */
 
-/* #region PIN SETUPS */
-#define VRX_PIN A0   // Arduino pin connected to VRX pin (aka Analog Pin A0), this is the blue cable
-#define VRY_PIN A1   // Arduino pin connected to VRY pin (aka Analog Pin A1), this is the red cable
-#define Solenoid 4   // Water Solenoid will be connected to Digital Pin 4
-#define Puff 10      // Air solenoid for blowing air in the face of the mouse will be connected to Pin 5
-#define REST_PIN 33  // Rest Pin will be connected to Digital Pin 33 on Behavior Teensy and Coordinate Teensy
-#define PUSH_PIN 34  // Push Pin will be connected to Digital Pin 34 on Behavior Teensy and Coordinate Teensy
-#define PULL_PIN 35  // Pull Pin will be connected to Digital Pin 35 on Behavior Teensy and Coordinate Teensy
-/* #endregion */
 
-/* #region LED Array Set Up*/
-//Parameters
-#define NUM_LEDS 48  //number of LEDs total
+
+#define VRX_PIN A0
+#define VRY_PIN A1
+#define Solenoid 4
+#define Puff 10
+#define REST_PIN 33
+#define PUSH_PIN 34
+#define PULL_PIN 35
+
+
+
+
+#define NUM_LEDS 48
 #define BRIGHTNESS 5
 
-// Set up
+
 #define DATA_PIN 8
 #define CLOCK_PIN 14
-#define NUM_LEDS 48  //number of LEDs total
+#define NUM_LEDS 48
 
-CRGB leds[NUM_LEDS];  //define the array of LEDs
-
-/* Array Set up (WS2012B)
-
-0  1  2  3  4  5
-6  7  8  9  10 11
-12 13 14 15 16 17
-18 19 20 21 22 23
-24 25 26 27 28 29
-30 31 32 33 34 35
-36 37 38 39 40 41
-42 43 44 45 46 47
-*/
-
-/* #endregion */
-
-
-/* #endregion */
-
-/* #region BEHAVIOR VARIBALES CONST */
+CRGB leds[NUM_LEDS];
+# 60 "C:/Users/rdh92/Documents/GitHub/Mouse-Push-Pull-Lever-Task/Code/everythingscript_clean/src/the_everything_script.ino"
 int totalTrials = 1000000;
 int trialNumber = 1;
 int decision;
@@ -65,42 +50,50 @@ int positionB = 0;
 int CorrectCounter = 0;
 int SOpenTime1;
 
-/* #end region */
 
-/* #region Timer Variables */
+
+
 unsigned long currentMillis = 0;
 int reactionTime;
 #define timerMillis millis() - currentMillis
 unsigned long startTime;
-
-/* #endregion */
-
+void setup();
+void loop();
+void printCueEvokedInformation(int isiDelay);
+void printVisualInformation(int isiDelay);
+void correctSOpen(int var);
+void printLightDecision();
+void printDecision();
+void printCorrectStatus();
+void printCorrectStatusMotor();
+void printOpenTime();
+#line 78 "C:/Users/rdh92/Documents/GitHub/Mouse-Push-Pull-Lever-Task/Code/everythingscript_clean/src/the_everything_script.ino"
 void setup() {
-  /* #region Pins + Serial */
+
   Serial.begin(115200);
   Serial1.begin(9600);
   pinMode(Solenoid, OUTPUT);
-  pinMode(Puff, OUTPUT);      // Sets the puff pin as an output
-  pinMode(29, OUTPUT);        // Sets pin 29 as an output, if this pin is high it prints on the Coordinate Teensy that the current state is the rest state
-  pinMode(30, OUTPUT);        // Sets pin 30 as an output, if this pin is high it prints on the Coordinate Teensy that the current state is the push/pull or push/down state
-  pinMode(31, OUTPUT);        // Sets pin 31 as an output, if this pin is high it prints on the Coordinate Teensy that the current state is that the solenoid is open
-  pinMode(32, OUTPUT);        // Sets pin 32 as an output, if this pin is high it prints on the Coordinate Teensy that the current state is that the ISI delay is occurring
-  pinMode(28, OUTPUT);        // Set pin 28 as an output, if this pin is high it prints on the Coordinate Teensy that the current state is punishment
+  pinMode(Puff, OUTPUT);
+  pinMode(29, OUTPUT);
+  pinMode(30, OUTPUT);
+  pinMode(31, OUTPUT);
+  pinMode(32, OUTPUT);
+  pinMode(28, OUTPUT);
   pinMode(25, OUTPUT);
-  pinMode(REST_PIN, INPUT);                             // Sets the rest pin as an input, if the mouse is in the rest coordinate range, the coordinate Teensy will set this pin high to tell the Behavior Teensy
-  pinMode(PULL_PIN, INPUT);                             // Sets the pull pin as an input, if the mouse is in the pull coordinate range, the coordinate Teensy will set this pin high to tell the Behavior Teensy
-  pinMode(PUSH_PIN, INPUT);                             // Sets the push pin as an input, if the mouse is in the push coordinate range, the coordinate Teensy will set this pin high to tell the Behavior Teensy                          // Probably useless
-  randomSeed(analogRead(9));                            // Needed for random number generator for ISI Delay
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // Sets up the light array
-  FastLED.setBrightness(BRIGHTNESS);                    // Sets the brightness of the light array
-  /* #endregion */
+  pinMode(REST_PIN, INPUT);
+  pinMode(PULL_PIN, INPUT);
+  pinMode(PUSH_PIN, INPUT);
+  randomSeed(analogRead(9));
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+
   Serial.println("What type of program would you like?");
   Serial.println("Type 1 for Visual Task");
   Serial.println("Type 2 for Motor Task");
   parseProgramTypeInput();
   Serial.print(programType);
   switch (programType) {
-    case 1: // visual task
+    case 1:
       Serial.println("Pick the Stage (1-4)");
       parseVisualStage();
       break;
@@ -115,7 +108,7 @@ void setup() {
 }
 
 void loop() {
-    // Rest position block
+
     currentMillis = millis();
     digitalWrite(29, HIGH);
     if (programType == 1) {
@@ -132,35 +125,35 @@ void loop() {
         continue;
       }
     }
-    digitalWrite(29, LOW);   // Tells the coordinate Teensy that the current state is not the rest state anymore
-    digitalWrite(30, HIGH);  // Tells the coordinate Teensy that the current state is the push/pull or push/down state
+    digitalWrite(29, LOW);
+    digitalWrite(30, HIGH);
     if (programType == 1) {
-      reactionTimeWindowModVisual(); // modify the reaction time window if necessary
-      lightArrayModifier(); // modify the percentages of light array options
-      turnOnTheLights(); // turn on the appropriate light array
+      reactionTimeWindowModVisual();
+      lightArrayModifier();
+      turnOnTheLights();
       }
     if (programType == 2) {
       play12kHzSound(soundCueDuration);
     }
-    currentMillis = millis(); // start the reaction time window timer
-    startTime = millis(); // start the reaction time timer
+    currentMillis = millis();
+    startTime = millis();
     while (timerMillis <= timeOutTime) {
       if (digitalRead(PUSH_PIN) == HIGH) {
-          decision = 1; // push
+          decision = 1;
           break;
         }
       if (digitalRead(PULL_PIN) == HIGH) {
-          decision = 2; // pull
+          decision = 2;
           break;
         } else {
-          decision = 0; // punishment
+          decision = 0;
         }
     }
-    reactionTime = millis() - startTime; // calculate the reaction time
-    lightsOff(); // turn off the lights - this can be in the motor task as well (doesn't hurt)
-    digitalWrite(30, LOW);  // Tells the coordinate Teensy that the current state is not the push/pull or push/down state anymore
-    // Decision Results block //
-    /* #region Visual Task Rewards */
+    reactionTime = millis() - startTime;
+    lightsOff();
+    digitalWrite(30, LOW);
+
+
     if (programType == 1) {
       switch (visualStage) {
         case 1:
@@ -223,8 +216,8 @@ void loop() {
           break;
       }
     }
-    /* #endregion */
-    /* #region Rewards for Cue Evoked Task */
+
+
     if (motorTaskType == 1) {
       switch (CueEvokedTaskDay) {
         case 1:
@@ -262,8 +255,8 @@ void loop() {
           }
       }
     }
-    /* #endregion */
-    // Punishment block //
+
+
     if (programType == 1 && decision == 0) {
       punishmentVisualTaskChooser();
     }
@@ -271,15 +264,15 @@ void loop() {
       wnPunishment(punishmentTime);
     }
     digitalWrite(28, LOW);
-    // ISI Delay block //
-    digitalWrite(32, HIGH);  // Tells the coordinate Teensy that the current state is that the ISI delay is occurring
+
+    digitalWrite(32, HIGH);
     if (programType == 1) {
       isiDelayModVisual();
     }
     int isiDelay = random(isiDelayLowerRange, isiDelayUpperRange + 1);
     delay(isiDelay);
-    digitalWrite(32, LOW);  // Tells the coordinate Teensy that the current state is not the ISI delay state anymore
-    // Print the data and increment variables as needed //
+    digitalWrite(32, LOW);
+
     if (programType == 1) {
       printVisualInformation(isiDelay);
     }
@@ -289,7 +282,7 @@ void loop() {
     trialNumber++;
 }
 
-/* #region CueEvokedTaskInformation */
+
 void printCueEvokedInformation(int isiDelay) {
   switch (CueEvokedTaskDay) {
     case 1: case 2:
@@ -334,10 +327,10 @@ void printCueEvokedInformation(int isiDelay) {
       break;
   }
 }
-/* #endregion */
 
 
-/* #region Visual Task Print Statements*/
+
+
 void printVisualInformation(int isiDelay) {
   switch (visualStage) {
     case 1:
@@ -363,7 +356,7 @@ void printVisualInformation(int isiDelay) {
       Serial.print(" , ");
       Serial.print(isiDelay);
       Serial.print(" , ");
-      // debug //
+
       Serial.print( " // ");
       Serial.print(" , ");
       Serial.print(holdTime);
@@ -396,7 +389,7 @@ void printVisualInformation(int isiDelay) {
       Serial.print(" , ");
       Serial.print(isiDelay);
       Serial.print(" , ");
-      // debug //
+
       Serial.print( " // ");
       Serial.print(" , ");
       Serial.print(plusPercentage);
@@ -435,7 +428,7 @@ void printVisualInformation(int isiDelay) {
       Serial.print(" , ");
       Serial.print(isiDelay);
       Serial.print(" , ");
-      // debug //
+
       Serial.print( " // ");
       Serial.print(" , ");
       Serial.print(plusPercentage);
@@ -474,7 +467,7 @@ void printVisualInformation(int isiDelay) {
       Serial.print(" , ");
       Serial.print(isiDelay);
       Serial.print(" , ");
-      // debug //
+
       Serial.print( " // ");
       Serial.print(" , ");
       Serial.print(plusPercentage);
@@ -587,5 +580,3 @@ void printOpenTime() {
       break;
   }
 }
-
-/* #endregion */
